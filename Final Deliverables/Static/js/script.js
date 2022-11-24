a149284d722d1f76c5d85be4d2dca0a05de6b3ff
stevey01 <114173140+stevey01@users.noreply.github.com>
2022-11-24 22:21:15 +0530

    Add files via upload

---
+++ Final Deliverables/Static/js/script.js
@@ -0,0 +1,46 @@
+function checkPassword(){
+    let password = document.getElementById("password").value;
+    let cnfrmPassword = document.getElementById("cnfrm-password").value;
+    console.log(" Password:", password,'\n',"Confirm Password:",cnfrmPassword);
+    let message = document.getElementById("message");
+
+    if(password.length != 0){
+        if(password != cnfrmPassword){
+            message.textContent = "Passwords didn't match";
+            message.style.backgroundColor = "#ff4d4d";
+        }
+    }
+    else{
+        alert("Password can't be empty!");
+        message.textContent = "";
+    }
+}
+
+
+const forms = document.querySelector(".forms"),
+      pwShowHide = document.querySelectorAll(".eye-icon"),
+      links = document.querySelectorAll(".link");
+
+pwShowHide.forEach(eyeIcon => {
+    eyeIcon.addEventListener("click", () => {
+        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+        
+        pwFields.forEach(password => {
+            if(password.type === "password"){
+                password.type = "text";
+                eyeIcon.classList.replace("bx-hide", "bx-show");
+                return;
+            }
+            password.type = "password";
+            eyeIcon.classList.replace("bx-show", "bx-hide");
+        })
+        
+    })
+})
+
+links.forEach(link => {
+    link.addEventListener("click", e => {
+       e.preventDefault(); //preventing form submit
+       forms.classList.toggle("show-signup");
+    })
+})
